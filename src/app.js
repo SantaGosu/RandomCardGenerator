a83f127f486da5d7f3ce2be3ea4b58cb32acec37
SantaGosu <taz.c.munoz@gmail.com>
2022-08-07 21:17:08 +0000

    Fixed first math.random to generate a value from array to get king queen etc.

--- src/app.js
+++ src/app.js
@@ -6,14 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function genCard() {
-  document.getElementById("Number").innerHTML = Math.floor(
-    Math.random() * 4 + 1
-  );
+  const arr = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "K",
+    "Q",
+    "J",
+    "A"
+  ];
+  document.getElementById("Number").innerHTML =
+    arr[Math.floor(Math.random() * arr.length)];
   let NumElement = document.getElementById("Number");
   NumElement.setAttribute(
     "style",
     "align-self: center; margin-top: 25%; font-size: xxx-large;"
   );
-  let CardIcons = document.getElementById("Icon");
-  CardIcons.innerHTML = Math.floor(Math.random() * 4 + 1);
+  // let CardIcons = document.getElementById("Icon");
+  // CardIcons.innerHTML = Math.floor(Math.random() * 4 + 1);
+
+  // const arr = ["one", "two", "three", "four", "five"];
+  // let random = document.getElementById("test2");
+  // random.innerHTML = arr[Math.floor(Math.random() * arr.length)];
+  // test.setAttribute("style", "margin: 0px; padding: 0px;");
 };
