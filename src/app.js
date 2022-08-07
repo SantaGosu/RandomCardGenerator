9ac9f699c082a44d40cb69c9d457d903c5088273
SantaGosu <taz.c.munoz@gmail.com>
2022-08-07 20:59:08 +0000

    setup second math.random function for the icons

--- src/app.js
+++ src/app.js
@@ -5,13 +5,15 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function genNumber() {
+window.onload = function genCard() {
   document.getElementById("Number").innerHTML = Math.floor(
-    Math.random() * 10 + 1
+    Math.random() * 4 + 1
   );
-  let test = document.getElementById("Number");
-  test.setAttribute(
+  let NumElement = document.getElementById("Number");
+  NumElement.setAttribute(
     "style",
     "align-self: center; margin-top: 25%; font-size: xxx-large;"
   );
+  let CardIcons = document.getElementById("Icon");
+  CardIcons.innerHTML = Math.floor(Math.random() * 4 + 1);
 };
